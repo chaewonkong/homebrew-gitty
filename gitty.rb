997bea4d1bcbf9b2e6ad7ac8f1853c0c34193aca
chaewonkong <chaewonkong@gmail.com>
2024-02-17 16:25:26 +0900

    fix: 🐛 checksum

--- gitty.rb
+++ gitty.rb
@@ -9,7 +9,7 @@ class Gitty < Formula
       sha256 "8b717523a9ddb548d2fd6009ca4f565160ece97f2d044e446f8c657e69fa75e8"
     elsif Hardware::CPU.arm?
       url "https://github.com/chaewonkong/gitty/releases/download/v1.0.7/gitty-macos-arm.tar.gz"
-      sha256 "8dd4215f8907c364cc9d1b5b8dd0bb253b71255949d9b3e6ab334cb43ef9e495"
+      sha256 "d4bc7c2a6076092e6fdbdbb24288c089baa595fb8cb40c3bfa6d8317726faf4d"
     end
   
     def install
